c8e07afb058e8f3faa20df85025cae39967d1b0a
youngAndMad <kaliaskaruly.d@adamants.kz>
2025-02-07 20:44:51 +0500

    no assets

--- ui/src/pages/MenuPage.tsx
+++ ui/src/pages/MenuPage.tsx
@@ -35,7 +35,6 @@ export default function MenuPage() {
         position: "relative",
       }}
     >
-      {/* Online users count at the top left */}
       <Box
         sx={{
           position: "absolute",
